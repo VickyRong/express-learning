4c63de18a0eae02981582f6f7f786d191987263c
huyanrong <515020842@qq.com>
2017-04-13 17:38:11 +0800

    修复post-demo无法获取数据的问题

--- post-demo/server.js
+++ post-demo/server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+ 
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 app.use(express.static('public'));
  
@@ -7,17 +11,22 @@ app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
  
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
  
    // 输出 JSON 格式
    response = {
-       first_name:req.query.first_name,
-       last_name:req.query.last_name
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
    };
    console.log(response);
    res.end(JSON.stringify(response));
 })
  
-app.listen(8081, function () {
-  console.log("应用实例，访问地址为8081")
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
 })
